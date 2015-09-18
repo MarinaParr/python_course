47032731d196609fb765c30259149535fecee63d
mparr <mparr@mail.ru>
2015-09-18 09:24:42 +0300

    dir added

---
+++ homeworks/hw1/СЛОВАРЬ.py
@@ -0,0 +1,17 @@
+#ÑËÎÂÀĞÜ********************************
+s=input()
+l=len(s)
+d=dict()
+lst=list()
+for i in range (0,l):
+    lst.append(s[i])
+lst.sort()
+i=0
+while i<l:
+    if lst[i] not in d:
+       d[lst[i]]=lst.count(lst[i])
+    i+=1
+l = list(d.keys())
+l.sort()
+for i in l:
+    print(i, d[i])
---
+++ homeworks/hw1/УТЮГИ И ЛОЖКИ.py
@@ -0,0 +1,33 @@
+#УТЮГИ И ЛОЖКИ*************************
+s=str(input())
+n=int(input())
+if s=='утюг':
+    if n%10==0:
+        print (str(n)+' утюгов')
+    if n%10==1:
+        if n%100!=11:
+            print (str(n)+' утюг')
+        else:
+            print (str(n)+' утюгов')
+    if 2<=n%10<=4:
+        if n%100==12 or n%100==13 or n%100==14:
+            print (str(n)+' утюгов')
+        else:
+            print (str(n)+' утюга')
+    if 5<=n%10<=9:
+        print (str(n)+' утюгов')
+if s=='ложка':
+    if n%10==0:
+        print (str(n)+' ложек')
+    if n%10==1:
+        if n%100!=11:
+            print (str(n)+' ложка')
+        else:
+            print (str(n)+' ложек')
+    if 2<=n%10<=4:
+        if n%100==12 or n%100==13 or n%100==14:
+            print (str(n)+' ложек')
+        else:
+            print (str(n)+' ложки')
+    if 5<=n%10<=9:
+        print (str(n)+' ложек')
