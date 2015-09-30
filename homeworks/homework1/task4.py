4ab39f2d5638dbdeda40c36b6f9621c244c0dcd6
Marina Parr <mparr@mail.ru>
2015-09-30 20:37:07 +0300

    Update task4.py

--- homeworks/homework1/task4.py
+++ homeworks/homework1/task4.py
@@ -1,15 +1,15 @@
-s=input()
-l=len(s)
-d=dict()
-lst=list()
-for i in range (0,l):
+s = input()
+l = len(s)
+d = dict()
+lst = list()
+for i in range(0, l):
     lst.append(s[i])
-lst.sort()
-i=0
-while i<l:
+lst.sort
+i = 0
+while i < l:
     if lst[i] not in d:
-       d[lst[i]]=lst.count(lst[i])
-    i+=1
+        d[lst[i]] = lst.count(lst[i])
+    i += 1
 l = list(d.keys())
 l.sort()
 for i in l:
