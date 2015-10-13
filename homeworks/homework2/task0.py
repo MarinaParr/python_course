4f92755ee21fb35d2089b9f998cc8ffc1fb45f0f
Marina Parr <mparr@mail.ru>
2015-10-13 18:55:03 +0300

    Update task0.py

--- homeworks/homework2/task0.py
+++ homeworks/homework2/task0.py
@@ -12,15 +12,15 @@ if subject == 'чайник':
 
 def plural(n, subjects):
     if n % 10 == 0 or (n % 10 == 1 and n % 100 == 11):
-        return (str(n) + " " + subjects[2])
+        return (subjects[2])
     if 2 <= n % 10 <= 4 and (n % 100 == 12 or n % 100 == 13 or n % 100 == 14):
-        return (str(n) + " " + subjects[2])
+        return (subjects[2])
     if 5 <= n % 10 <= 9:
-        return (str(n) + " " + subjects[2])
+        return (subjects[2])
     if n % 10 == 1 and n % 100 != 11:
-        return (str(n) + " " + subjects[0])
+        return (subjects[0])
     if 2 <= n % 10 <= 4 and not n % 100 == 12:
-        return (str(n) + " " + subjects[1])
+        return (subjects[1])
     if 2 <= n % 10 <= 4 and not n % 100 == 13 or n % 100 == 14:
-        return (str(n) + " " + subjects[1])
-print(plural(n, subjects))
+        return (subjects[1])
+print(str(n) + " " + plural(n, subjects))
