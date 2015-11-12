02688f566da4f69782348d7a5453e8c0cd4f420b
Marina Parr <mparr@mail.ru>
2015-11-12 22:49:16 +0300

    Update task1.py

--- exam/task1.py
+++ exam/task1.py
@@ -1,23 +1,11 @@
-# попытка эффективно по памяти прочитать
-f = open("yazkora.txt", "r")
-for line in f:
-    adjectives = str()
-    words = line.split(" ")
-    for word in words:
-        print(word)
-        if word[len(word)-1] == "o" and word[len(word)-2] == "y":
-            adjectives += word
-    print(adjectives)
-
-
-# хоть как-нибудь считать
 with open("yazkora.txt", "r") as f:
-    text = f.read()
-propositions = text.split(".")
-for proposition in propositions:
-    adjectives = str()
-    words = proposition.split(" ")
-    for word in words:
-        if word[len(word)-1] == "o" and word[len(word)-2] == "y":
-            adjectives += word
-    print(adjectives)
+    with open("answer.txt", "w")as answ:
+        text = f.read()
+        propositions = text.split(".")
+        for proposition in propositions:
+            words = proposition.split(" ")
+            adjectives = str()
+            for word in words:
+                if word[-2:] == "yo":
+                    adjectives += word + " "
+            answ.write(adjectives)
