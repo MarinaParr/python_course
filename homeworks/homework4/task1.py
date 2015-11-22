7c508df93382033357fa9b629f981cd150a7dd74
Marina Parr <mparr@mail.ru>
2015-11-22 14:04:40 +0300

    Update task1.py

--- homeworks/homework4/task1.py
+++ homeworks/homework4/task1.py
@@ -1,6 +1,6 @@
 with open("yazkora.txt", "r") as f:
     with open("answer.txt", "w")as answ:
-        text = f.read()
+        text = f.read().replace("\n", " ")
         propositions = text.split(".")
         for proposition in propositions:
             words = proposition.split(" ")
@@ -8,4 +8,4 @@ with open("yazkora.txt", "r") as f:
             for word in words:
                 if word[-2:] == "yo":
                     adjectives += word + " "
-            answ.write(adjectives)
+            answ.write(adjectives + "\n")
