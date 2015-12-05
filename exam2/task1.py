1230d922479c9f19dc7a55615122ad3b3649be4e
Marina Parr <mparr@mail.ru>
2015-12-05 12:25:40 +0300

    Update task1.py

--- exam2/task1.py
+++ exam2/task1.py
@@ -4,6 +4,8 @@ import re
 with open("hp5.txt", "r") as f:
     text = f.read().replace("\n", " ")
     whispers_up_2 = re.findall('([A-Z]\w+ [A-Z]\w+ whispered)', text)
+# Я понимаю, что таким способом мы Aunt Petunia и Petunia учитываем дважды,
+# но лучше придумать не могу пока
     whispers_up_1 = re.findall('([A-Z]\w+ whispered)', text)
     whispers_down_1 = re.findall('(whispered [A-Z]\w+)', text)
     whispers_down_2 = re.findall('(whispered [A-Z]\w+ [A-Z]\w+)', text)
