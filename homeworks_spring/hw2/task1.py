a94058edde7220d47787d2b24c27f056fe850f78
Marina Parr <mparr@mail.ru>
2016-03-06 09:24:00 +0300

    Update task1.py

--- homeworks_spring/hw2/task1.py
+++ homeworks_spring/hw2/task1.py
@@ -1,1 +1,12 @@
-
+l = list()
+if issubclass(D, A):
+    l.append("A")
+if issubclass(D, B):
+    l.append("B")
+if issubclass(D, C):
+    l.append("C")
+l.sort()
+s = str()
+for i in range(0, len(l)):
+    s += l[i] + ' '
+print(s)
