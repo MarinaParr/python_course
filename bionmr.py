518acb13d518553dd9c63eed6d2af293096066cd
Marina Parr <mparr@mail.ru>
2016-06-27 12:58:49 +0400

    Update bionmr.py

--- bionmr.py
+++ bionmr.py
@@ -11,10 +11,10 @@ def making_table(file):
             numbers = [  map(float,s.strip().split(" ")) for s in strings[1:]]
             l = []
             for int_name in int_names:
-                inf_about_int = re.split('~|::|--', int_name)
-                inf_about_int[1] = str(int((inf_about_int[1]))//76)
-                inf_about_int[4] = str(int((inf_about_int[4]))//76)
-                name = '-'.join(inf_about_int)
+                inf = re.split('~|::|--', int_name)
+                inf[1] = str(int((inf[1]))//76)
+                inf[4] = str(int((inf[4]))//76)
+                name = inf[0]+"~"+inf[1]+"::"+inf[2]+"--"+inf[3]+"~"+inf[4]+"::"+inf[5]
                 l.append(name)
             t.write('\t'.join(l)+"\n")
             length = len(numbers[0])
@@ -30,7 +30,6 @@ def making_table(file):
                     k += 1
                 t.write("\t".join(str(x) for x in l)+"\n")
 
-
 def reverse_table(file):
     with open(file, "r") as tab:
         with open("rev_"+file, "w") as rt:
@@ -40,8 +39,32 @@ def reverse_table(file):
                     rt.write(y+'\t')
                 rt.write('\n')
 
+def partition_and_sorting(file):
+    with open(file, "r") as rt:
+        strings = rt.readlines()
+        first_intss = dict()
+        for string in strings[:len(strings)-1]:
+            data = string.split("\t")
+            interactors = data[0]
+            first_interactor = re.split('~|::|--',interactors)[1]
+            if first_interactor not in first_intss:
+                first_intss[first_interactor] = list()
+                first_intss[first_interactor].append(string)
+            else:
+                first_intss[first_interactor].append(string)
+        for key in first_intss:
+            with open(key+"_inner_"+file, "w") as inner:
+                with open(key+"_outer_"+"_"+file, "w") as outer:
+                    for elem in first_intss[key]:
+                        second_interactor = re.split('~|::|--',(elem.split("\t")[0]))[4]
+                        if second_interactor == key:
+                            inner.write(elem+"\n")
+                        else:
+                            outer.write(elem+"\n")
+
 with open("list_of_files.txt", "r") as lf:
     files_names = lf.read().split("\n")
     for file_name in  filter(None, files_names):
         making_table(file_name)
         reverse_table("tab_"+file_name)
+        partition_and_sorting("rev_"+"tab_"+file_name)
